5e1e3f7203da76c282ac6f7a05dcbb4b7cde6a66
danial_derayati <88569925+iamdanial79@users.noreply.github.com>
2021-11-11 09:24:22 +0330

    add models and register to admin panel

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
     'my_blog'
 ]
 
@@ -121,9 +122,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+import os
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+
+
+CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
+CKEDITOR_UPLOAD_PATH = "uploads/"
--- blog/my_blog/models.py
+++ blog/my_blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField
+from ckeditor.fields import RichTextField
+from datetime import datetime
 
-# Create your models here.
+#add user field
+class userprofile(models.Model):
+    user = models.OneToOneField( User,on_delete=models.CASCADE )
+    avatar = models.ImageField( upload_to="files/user_avatar/", null=True , blank=True )
+    description = models.CharField(max_length=5000)
+    def __str__(self) :
+        return self.user.first_name
+#add article field
+class article(models.Model):
+    title = models.CharField( max_length=200 , null=False , blank=False )
+    cover = models.ImageField( upload_to='files/article_cover/' , null=False , blank=False )
+    content = RichTextField()
+    created_at = models.DateTimeField(default=datetime.now , blank=False)
+    category = models.ForeignKey('Category',on_delete=models.CASCADE)
+    author = models.OneToOneField(userprofile, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+        
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False)
+    cover = models.ImageField(upload_to='files/category_cover/' )
+    def __str__(self):
+        return self.title
